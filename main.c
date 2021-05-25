f772b5eaae0c9f80a6ed8674aa1b166c776362de
jianghongbin <jianghongbin@cmcm.com>
2021-05-25 21:27:23 +0800

    feat: zip_decode_len

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "ziplist.h"
-
+#include <stdlib.h>
+#include <memory.h>
 int main() {
     printf("Hello, World!\n");
 
@@ -10,5 +11,16 @@ int main() {
     printf("zl tail offset = %d\n", *((uint32_t*)(zl+sizeof(uint32_t))));
     printf("zl len len = %d\n", *((uint16_t *)(zl+sizeof(uint32_t)*2)));
     printf("zl end = %d\n", *((uint8_t *)(zl+sizeof(uint32_t)*2+sizeof(uint16_t))));
+
+    char buf[4] = {0x04, 0x03, 0x02, 0x01};
+    uint32_t *ptr = &buf;
+    printf("%d\n", *ptr);
+
+    unsigned int p;
+    memcpy(&p, buf, 4);
+    printf("%d\n", p);
+
+    unsigned char *encoding = (unsigned char *)(buf+1);
+    printf("%d\n", encoding[0]);
     return 0;
 }
